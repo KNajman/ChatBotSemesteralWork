8928dd9abe976d41e2498e11b5835effb412177d
KNajman <karel.najman@tul.cz>
2022-05-05 01:11:20 +0200

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -25,7 +25,7 @@ function sendReply() {
     document.getElementById('chat-input').value = ""
     document.getElementById('inputbox').scrollIntoView({ block: 'end', behavior: 'smooth' })
 
-    fetch('http://127.0.0.1:5000/predict', {
+    fetch('http://10.201.241.34:5000/predict', {
             method: 'POST',
             body: JSON.stringify({ message: message }),
             mode: 'cors',
